11993b62eca3cda66e087208a0a73f892a5c5b08
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-04-16 12:41:53 +0200

    added delete method

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,56 @@
 let app = new Vue({
-    el: "#app",
-    data: {
-        storeName: "",
-        products: [],
-    },
-    methods: {
-        apiStore: function () {
-            axios
-                .get("http://us-central1-test-b7665.cloudfunctions.net/api/stores/ijpxNJLM732vm8AeajMR")
-                .then((response) => {
-                    this.storeName = response.data.name
-                    console.log(this.storeName)
-                })
-                .catch((error) => {
-                    alert(error);
-                });
-        },
-        apiProducts: function () {
-            axios
-                .get("http://us-central1-test-b7665.cloudfunctions.net/api/stores/ijpxNJLM732vm8AeajMR/products")
-                .then((response) => {
-                    this.products = response.data
-                    console.log(this.products)
-                })
-                .catch((error) => {
-                    alert(error);
-                });
-        },
-    },
-    mounted() {
-        this.apiStore();
-        this.apiProducts();
-    },
+	el: "#app",
+	data: {
+		storeName: "",
+		products: [],
+		id: "",
+	},
+	methods: {
+		apiStore: function () {
+			axios
+				.get(
+					"http://us-central1-test-b7665.cloudfunctions.net/api/stores/ijpxNJLM732vm8AeajMR"
+				)
+				.then((response) => {
+					this.storeName = response.data.name;
+				})
+				.catch((error) => {
+					alert(error);
+				});
+		},
+		apiProducts: function () {
+			axios
+				.get(
+					"http://us-central1-test-b7665.cloudfunctions.net/api/stores/ijpxNJLM732vm8AeajMR/products"
+				)
+				.then((response) => {
+					this.products = response.data;
+				})
+				.catch((error) => {
+					alert(error);
+				});
+		},
+		deleteData: function (id) {
+			this.id = id;
+			// console.log(id);
+			axios
+				.delete(
+					"http://us-central1-test-b7665.cloudfunctions.net/api/stores/ijpxNJLM732vm8AeajMR/products/" +
+						this.id
+				)
+				.catch((error) => {
+					alert(error);
+				});
+			location.reload();
+		},
+		/* 		postData: function {
+			axios.post(
+				""
+			)
+		} */
+	},
+	mounted() {
+		this.apiStore();
+		this.apiProducts();
+	},
 });
